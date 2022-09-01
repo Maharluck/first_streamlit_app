94d14d18befa596c7efb6f31470fef41fdecac5b
Maharluck <111665439+Maharluck@users.noreply.github.com>
2022-09-01 12:34:07 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 streamlit.title('My parents first healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text(https://learn.snowflake.com/assets/courseware/v1/ebfb9d6559f4e8b3edb15255de776037/asset-v1:snowflake+ESS-DABW+A+type@asset+block/DABW_42.png & Omega 3 Blueberry Oatmeal')
+streamlit.text(https://learn.snowflake.com/assets/courseware/v1/ebfb9d6559f4e8b3edb15255de776037/asset-v1:snowflake+ESS-DABW+A+type@asset+block/DABW_42.png & 'Omega 3 Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
